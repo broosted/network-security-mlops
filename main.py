bfdf12bc888f65f2cedf32072872e1a26db37c45
Tarik Tad <matrix.10.tad@gmail.com>
2025-05-16 10:39:33 -0700

    added data ingestion step

---
+++ main.py
@@ -0,0 +1,22 @@
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
+
+import sys
+
+
+
+
+if __name__ == "__main__":
+    try:
+        logging.info("Starting data ingestion")
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        logging.info("Data ingestion initiated")
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion completed")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
